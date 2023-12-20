f4d15a0f32f63100423d3a80c4c83a913f139a71
Anukul <copopoco@gmail.com>
2023-12-21 02:01:52 +0530

    init

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 4000
+const gitData = {
+    "login": "AnukulSaxena",
+    "id": 101214911,
+    "node_id": "U_kgDOBghqvw",
+    "avatar_url": "https://avatars.githubusercontent.com/u/101214911?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/AnukulSaxena",
+    "html_url": "https://github.com/AnukulSaxena",
+    "followers_url": "https://api.github.com/users/AnukulSaxena/followers",
+    "following_url": "https://api.github.com/users/AnukulSaxena/following{/other_user}",
+    "gists_url": "https://api.github.com/users/AnukulSaxena/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/AnukulSaxena/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/AnukulSaxena/subscriptions",
+    "organizations_url": "https://api.github.com/users/AnukulSaxena/orgs",
+    "repos_url": "https://api.github.com/users/AnukulSaxena/repos",
+    "events_url": "https://api.github.com/users/AnukulSaxena/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/AnukulSaxena/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Anukul Prakash Saxena",
+    "company": null,
+    "blog": "",
+    "location": "Uttarakhand, India",
+    "email": null,
+    "hireable": null,
+    "bio": "Why CS is so addictive... ",
+    "twitter_username": null,
+    "public_repos": 6,
+    "public_gists": 0,
+    "followers": 3,
+    "following": 4,
+    "created_at": "2022-03-08T19:51:33Z",
+    "updated_at": "2023-12-10T13:29:06Z"
+}
 
 app.get('/', (req, res) => {
     res.send("hello")
@@ -11,6 +45,10 @@ app.get('/', (req, res) => {
 
 app.get('/twitter', (req, res) => {
     res.send('Twitter')
+})
+
+app.get('/github', (req, res) => {
+    res.json(gitData)
 })
 
 app.listen(port, () => {
