d8c337901615952b8b241e9c074838d54fdf41f5
Anukul <copopoco@gmail.com>
2023-12-22 01:53:16 +0530

    MongoDB Integration

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect
+            (`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("MongoDB Connnected")
+    } catch (err) {
+        console.log("MongoDB ERROR : ", err);
+        process.exit(1)
+    }
+}
+
+export default connectDB
