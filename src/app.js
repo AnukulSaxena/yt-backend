2452ecacfac7512960f44885303ba28aad8ed58e
Anukul Prakash Saxena <anukulsaxena83@gmail.com>
2024-02-15 09:25:27 +0530

    test

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express()
 const corsOptions = {
     origin: ['http://localhost:5173'],
     credentials: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
