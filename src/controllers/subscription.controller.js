7545aa193bd7ffbd1e62a5d2546d43a26e1eec57
Anukul Prakash Saxena <anukulsaxena83@gmail.com>
2024-03-07 18:38:49 +0530

    refactor : subscription validators

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,140 +1,137 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import { User } from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import { ApiError } from "../utils/ApiError.js"
-import { ApiResponse } from "../utils/ApiResponse.js"
-import { asyncHandler } from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const { channelId } = req.params
-    const { user } = req;
+  const { channelId } = req.params;
+  const { user } = req;
 
-    if (channelId === String(user?._id)) {
-        throw new ApiError(400, "Invalid Operation")
-    }
+  if (channelId === String(user?._id)) {
+    throw new ApiError(400, "Invalid Operation");
+  }
 
-    let subscription;
-    let message = "";
-    try {
-        subscription = await Subscription.findOneAndDelete({
-            subscriber: user._id,
-            channel: channelId
-        })
-        message = "Unsubscribed successfully"
-    } catch (error) {
-        throw new ApiError(400, "Invalid ChannelId")
-    }
+  let message = "";
+  const subscription = await Subscription.findOneAndDelete({
+    subscriber: user._id,
+    channel: channelId,
+  });
+  message = "Unsubscribed successfully";
 
-    if (!subscription) {
-        subscription = await Subscription.create({
-            subscriber: user._id,
-            channel: channelId
-        })
-        message = "Subscribed successfully"
-    }
-    if (!subscription) {
-        throw new ApiError(500, "Something went wrong during subscribing")
-    }
+  if (!subscription) {
+    subscription = await Subscription.create({
+      subscriber: user._id,
+      channel: channelId,
+    });
+    message = "Subscribed successfully";
+  }
+  if (!subscription) {
+    throw new ApiError(500, "Something went wrong during subscribing");
+  }
 
-    res.status(200).json(
-        new ApiResponse(200, subscription, message)
-    )
-})
+  res.status(200).json(new ApiResponse(200, subscription, message));
+});
 
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const { channelId } = req.params;
+  const { channelId } = req.params;
 
-    let channels;
-    try {
-        channels = await Subscription.aggregate([
-            {
-                '$match': {
-                    'channel': new mongoose.Types.ObjectId(channelId)
-                }
-            }, {
-                '$lookup': {
-                    'from': 'users',
-                    'localField': 'subscriber',
-                    'foreignField': '_id',
-                    'as': 'channelSubscribers'
-                }
-            }, {
-                '$unwind': '$channelSubscribers'
-            }, {
-                '$replaceRoot': {
-                    'newRoot': '$channelSubscribers'
-                }
-            }
-        ])
-    } catch (error) {
-        throw new ApiError(400, "Invalid ChannelId")
-    }
+  const channels = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "channelSubscribers",
+      },
+    },
+    {
+      $unwind: "$channelSubscribers",
+    },
+    {
+      $replaceRoot: {
+        newRoot: "$channelSubscribers",
+      },
+    },
+  ]);
 
-    res.status(200).json(
-        new ApiResponse(200, channels, "Channel subscribers fetched successfully")
-    )
-})
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, channels, "Channel subscribers fetched successfully")
+    );
+});
 
 const getUserChannelSubscribersCount = asyncHandler(async (req, res) => {
-    const { channelId } = req.params;
+  const { channelId } = req.params;
 
-    let channels;
-    try {
-        channels = await Subscription.find({
-            channel: channelId
-        })
-    } catch (error) {
-        throw new ApiError(400, "Invalid ChannelId")
-    }
+  const channels = await Subscription.find({
+    channel: channelId,
+  });
 
-    const isSubscribed = channels.some(item => String(item.subscriber) === String(req.user._id))
+  const isSubscribed = channels.some(
+    (item) => String(item.subscriber) === String(req.user._id)
+  );
 
-    res.status(200).json(
-        new ApiResponse(200, {
-            length: channels?.length,
-            isSubscribed
-        }, "Channel subscribers fetched successfully")
+  res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        length: channels?.length,
+        isSubscribed,
+      },
+      "Channel subscribers Count fetched successfully"
     )
-})
+  );
+});
 
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { user } = req;
+  const { user } = req;
 
-    let subscribers;
-    try {
-        subscribers = await Subscription.aggregate([
-            {
-                '$match': {
-                    'subscriber': user._id
-                }
-            }, {
-                '$lookup': {
-                    'from': 'users',
-                    'localField': 'channel',
-                    'foreignField': '_id',
-                    'as': 'subscribedChannels'
-                }
-            }, {
-                '$unwind': '$subscribedChannels'
-            }, {
-                '$replaceRoot': {
-                    'newRoot': '$subscribedChannels'
-                }
-            }
-        ])
-    } catch (error) {
-        throw new ApiError(400, "Invalid ChannelId")
-    }
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: user._id,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "subscribedChannels",
+      },
+    },
+    {
+      $unwind: "$subscribedChannels",
+    },
+    {
+      $replaceRoot: {
+        newRoot: "$subscribedChannels",
+      },
+    },
+  ]);
 
-    res.status(200).json(
-        new ApiResponse(200, subscribers, "Channel subscribers fetched successfully")
-    )
-})
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscribers,
+        "Channel subscribers fetched successfully"
+      )
+    );
+});
 
 export {
-    toggleSubscription,
-    getUserChannelSubscribers,
-    getSubscribedChannels,
-    getUserChannelSubscribersCount
-}
+  toggleSubscription,
+  getUserChannelSubscribers,
+  getSubscribedChannels,
+  getUserChannelSubscribersCount,
+};
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,24 +1,38 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { validate } from "../validators/validate.js";
+import { mongoIdPathVariableValidator } from "../validators/common/mongodb.validators.js";
 import {
-    getSubscribedChannels,
-    getUserChannelSubscribers,
-    getUserChannelSubscribersCount,
-    toggleSubscription,
-} from "../controllers/subscription.controller.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  getUserChannelSubscribersCount,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router
-    .route("/c/:channelId")
-    .get(getUserChannelSubscribers)
-    .post(toggleSubscription);
+  .route("/c/:channelId")
+  .get(
+    mongoIdPathVariableValidator("channelId"),
+    validate,
+    getUserChannelSubscribers
+  )
+  .post(
+    mongoIdPathVariableValidator("channelId"),
+    validate,
+    toggleSubscription
+  );
 
 router
-    .route('/c/check/:channelId')
-    .get(getUserChannelSubscribersCount)
+  .route("/c/check/:channelId")
+  .get(
+    mongoIdPathVariableValidator("channelId"),
+    validate,
+    getUserChannelSubscribersCount
+  );
 
 router.route("/u/").get(getSubscribedChannels);
 
-export default router
+export default router;
